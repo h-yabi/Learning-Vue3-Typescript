ee5cfc2f59fb329d5a604ccc594e12c77acc5889
h_yabiku <>
2022-05-02 13:33:34 +0900

    カスタムディレクティブに modifiers 設定

--- vue-sample/src/components/Payment.vue
+++ vue-sample/src/components/Payment.vue
@@ -1,15 +1,20 @@
 <script setup lang="ts">
+import { DirectiveBinding } from 'vue';
+
 // v-focus
 const vFocus = {
-  mounted: (el: HTMLElement) => {
+  mounted: (el: HTMLElement, binding: DirectiveBinding) => {
     el.focus();
+    if (binding.modifiers.alert) {
+      el.style.backgroundColor = 'pink';
+    }
   },
 };
 </script>
 
 <template>
   <div>Payment</div>
-  <input type="text" v-focus />
+  <input type="text" v-focus.alert />
 </template>
 
 <style scoped></style>
