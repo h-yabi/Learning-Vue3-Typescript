dff7cd590dec5bcbedbca45bed3fd426f7400504
h_yabiku <>
2022-05-02 12:04:03 +0900

    Provide, Inject を使ってみた

--- vue-sample/src/App.vue
+++ vue-sample/src/App.vue
@@ -1,9 +1,19 @@
 <script setup lang="ts">
-import Reservation from './components/Reservation.vue';
+import { provide } from 'vue';
+import TodoList from './components/TodoList.vue';
+import { todos, todoKey } from './useTodo';
+// import { todos } from './useTodo';
+
+// ▼ 1つずつ書く場合
+// provide(todoKey, todos);
+// provide(todoKey, addTodo);
+
+// ▼ まとめて書くこともできる
+provide(todoKey, todos);
 </script>
 
 <template>
-  <Reservation></Reservation>
+  <TodoList />
 </template>
 
 <style>
@@ -13,6 +23,5 @@ import Reservation from './components/Reservation.vue';
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
