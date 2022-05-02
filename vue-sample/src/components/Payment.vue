ad48e5c92628277f1ae33bce1f2d7cc49ea9f65d
h_yabiku <>
2022-05-02 13:14:44 +0900

    カスタムディレクティブ

--- vue-sample/src/components/Payment.vue
+++ vue-sample/src/components/Payment.vue
@@ -1,7 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// v-focus
+const vFocus = {
+  mounted: (el: HTMLElement) => {
+    el.focus();
+  },
+};
+</script>
 
 <template>
   <div>Payment</div>
+  <input type="text" v-focus />
 </template>
 
 <style scoped></style>
